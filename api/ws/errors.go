cd5e3b09a6fdd73b53e3d8ae70cc470d9073ba9f
Peter Nemere <peter.nemere@qut.edu.au>
2023-06-26 18:17:59 +1000

    Fixed error status reporting for web socket responses

--- api/ws/errors.go
+++ api/ws/errors.go
@@ -1,6 +1,8 @@
 package ws
 
 import (
+	"net/http"
+
 	"github.com/pixlise/core/v3/core/errorwithstatus"
 	protos "github.com/pixlise/core/v3/generated-protos"
 )
@@ -12,9 +14,18 @@ func makeRespStatus(err error) protos.ResponseStatus {
 		// we say WS_SERVER_ERROR
 		status := e.Status()
 		if status <= int(protos.ResponseStatus_WS_UNDEFINED.Number()) || status > int(protos.ResponseStatus_WS_SERVER_ERROR.Number()) {
-			return protos.ResponseStatus_WS_SERVER_ERROR
+			// Try to map errors we'd send back via HTTP
+			if status == http.StatusNotFound {
+				return protos.ResponseStatus_WS_NOT_FOUND
+			} else if status == http.StatusBadRequest {
+				return protos.ResponseStatus_WS_BAD_REQUEST
+			} else if status == http.StatusUnauthorized {
+				return protos.ResponseStatus_WS_NO_PERMISSION
+			} else {
+				return protos.ResponseStatus_WS_SERVER_ERROR
+			}
 		}
-		return protos.ResponseStatus(e.Status())
+		return protos.ResponseStatus(status)
 	default:
 		return protos.ResponseStatus_WS_SERVER_ERROR
 	}
