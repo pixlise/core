264ad88c9f75bf1ac25d025e7ec8abf284e00acc
Peter Nemere <peter.nemere@qut.edu.au>
2023-09-27 08:56:53 +1000

    Merged

---
+++ api/ws/handlers/doi.go
@@ -0,0 +1,14 @@
+package wsHandler
+
+import (
+	"errors"
+	protos "github.com/pixlise/core/v3/generated-protos"
+	"github.com/pixlise/core/v3/api/ws/wsHelpers"
+)
+
+func HandlePublishExpressionToZenodoReq(req *protos.PublishExpressionToZenodoReq, hctx wsHelpers.HandlerContext) (*protos.PublishExpressionToZenodoResp, error) {
+    return nil, errors.New("HandlePublishExpressionToZenodoReq not implemented yet")
+}
+func HandleZenodoDOIGetReq(req *protos.ZenodoDOIGetReq, hctx wsHelpers.HandlerContext) (*protos.ZenodoDOIGetResp, error) {
+    return nil, errors.New("HandleZenodoDOIGetReq not implemented yet")
+}
