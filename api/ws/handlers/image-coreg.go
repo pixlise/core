d7ab6c024455853825502d8e84a6269937f2c840
Peter Nemere <peter.nemere@qut.edu.au>
2023-12-13 11:32:50 +1000

    Adding image coreg messaging

---
+++ api/ws/handlers/image-coreg.go
@@ -0,0 +1,11 @@
+package wsHandler
+
+import (
+	"errors"
+	protos "github.com/pixlise/core/v3/generated-protos"
+	"github.com/pixlise/core/v3/api/ws/wsHelpers"
+)
+
+func HandleImportMarsViewerImageReq(req *protos.ImportMarsViewerImageReq, hctx wsHelpers.HandlerContext) (*protos.ImportMarsViewerImageResp, error) {
+    return nil, errors.New("HandleImportMarsViewerImageReq not implemented yet")
+}
