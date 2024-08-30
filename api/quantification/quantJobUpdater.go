34c70bcaff2c9f1daefc03b3aebc55ec167f0046
Peter Nemere <peter.nemere@qut.edu.au>
2024-08-30 11:23:58 +1000

    Fixing quant fit - now treated as a normal quant job, so we send back a response when creating and data arrives in a later update message. This required changing some of the customisations around how a job is created, while checking if command is 'map' or not

--- api/quantification/quantJobUpdater.go
+++ api/quantification/quantJobUpdater.go
@@ -4,8 +4,10 @@ import (
 	"fmt"
 
 	"github.com/olahol/melody"
+	"github.com/pixlise/core/v4/api/filepaths"
 	"github.com/pixlise/core/v4/api/services"
 	"github.com/pixlise/core/v4/api/ws/wsHelpers"
+	"github.com/pixlise/core/v4/core/fileaccess"
 	"github.com/pixlise/core/v4/core/scan"
 	protos "github.com/pixlise/core/v4/generated-protos"
 	"go.mongodb.org/mongo-driver/mongo"
@@ -15,8 +17,10 @@ type QuantJobUpdater struct {
 	params  *protos.QuantCreateParams
 	session *melody.Session
 	//melody   *melody.Melody
-	notifier services.INotifier
-	db       *mongo.Database
+	notifier    services.INotifier
+	db          *mongo.Database
+	fs          fileaccess.FileAccess
+	usersBucket string
 }
 
 func MakeQuantJobUpdater(
@@ -25,16 +29,21 @@ func MakeQuantJobUpdater(
 	//melody *melody.Melody,
 	notifier services.INotifier,
 	db *mongo.Database,
+	fs fileaccess.FileAccess,
+	usersBucket string,
 ) QuantJobUpdater {
 	return QuantJobUpdater{
 		params:  params,
 		session: session,
 		//melody:   melody,
-		notifier: notifier,
-		db:       db,
+		notifier:    notifier,
+		db:          db,
+		fs:          fs,
+		usersBucket: usersBucket,
 	}
 }
 
+// We send updates for quant jobs (that are long running, have names, stick around) this way...
 func (i *QuantJobUpdater) SendQuantJobUpdate(status *protos.JobStatus) {
 	if i.session != nil {
 		wsUpd := protos.WSMessage{
@@ -58,5 +67,28 @@ func (i *QuantJobUpdater) SendQuantJobUpdate(status *protos.JobStatus) {
 
 		i.notifier.NotifyNewQuant(false, status.JobId, i.params.Name, "Complete", scan.Title, i.params.ScanId)
 		i.notifier.SysNotifyQuantChanged(status.JobId)
+	}
+}
+
+// We send updates for ephemeral quant jobs (that are short running) this way...
+func (i *QuantJobUpdater) SendEphemeralQuantJobUpdate(status *protos.JobStatus) {
+	// We only send out an update for a completed job...
+	if status.Status == protos.JobStatus_COMPLETE && i.session != nil {
+		// We send out the result data, as opposed to a status
+		userOutputFilePath := filepaths.GetUserLastPiquantOutputPath(status.RequestorUserId, i.params.ScanId, i.params.Command, filepaths.QuantLastOutputFileName+".csv")
+		bytes, err := i.fs.ReadObject(i.usersBucket, userOutputFilePath)
+		if err != nil {
+			fmt.Errorf("PIQUANT job ephermal status failed to find output for: %v", status.JobId)
+		}
+
+		wsUpd := protos.WSMessage{
+			Contents: &protos.WSMessage_QuantCreateUpd{
+				QuantCreateUpd: &protos.QuantCreateUpd{
+					ResultData: bytes,
+				},
+			},
+		}
+
+		wsHelpers.SendForSession(i.session, &wsUpd)
 	}
 }
