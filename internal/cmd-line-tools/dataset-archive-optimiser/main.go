3408227af6066cffc057ac5b8fa4b08f0e2cf955
Peter Nemere <peter.nemere@qut.edu.au>
2024-11-08 10:47:53 +1000

    Added archive optimiser update mode, so we can copy back to the bucket. This is temporary

--- internal/cmd-line-tools/dataset-archive-optimiser/main.go
+++ internal/cmd-line-tools/dataset-archive-optimiser/main.go
@@ -3,14 +3,17 @@ package main
 import (
 	"archive/zip"
 	"context"
+	"errors"
 	"flag"
 	"fmt"
 	"log"
 	"os"
 	"path"
 	"path/filepath"
+	"strings"
 	"time"
 
+	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/pixlise/core/v4/api/dataimport/datasetArchive"
 	"github.com/pixlise/core/v4/api/dbCollections"
 	"github.com/pixlise/core/v4/core/awsutil"
@@ -25,18 +28,22 @@ import (
 
 var t0 = time.Now().UnixMilli()
 
+var destMongoSecret string
+var dbName string
+var sourceDataBucket string
+var destDataBucket string
+var updateScript string
+
 func main() {
 	fmt.Printf("Started: %v\n", time.Now().String())
 
-	var destMongoSecret string
-	var dbName string
-	var sourceDataBucket string
-	var destDataBucket string
+	var mode string
 
 	flag.StringVar(&destMongoSecret, "destMongoSecret", "", "Destination mongo DB secret")
 	flag.StringVar(&dbName, "dbName", "", "DB name we're importing to")
 	flag.StringVar(&sourceDataBucket, "sourceDataBucket", "", "Data bucket so we can read archive zips")
 	flag.StringVar(&destDataBucket, "destDataBucket", "", "Data bucket we're writing optimised archives to")
+	flag.StringVar(&mode, "mode", "", "combine (to download zips and combine them into one) or update (to put the combined zips back to the source)")
 
 	flag.Parse()
 
@@ -44,13 +51,16 @@ func main() {
 	checkNotEmpty := []string{
 		sourceDataBucket,
 		destDataBucket,
-		dbName,
 	}
 	checkNotEmptyName := []string{
 		"sourceDataBucket",
 		"destDataBucket",
-		"dbName",
 	}
+	if mode == "combine" {
+		checkNotEmptyName = append(checkNotEmptyName, "dbName")
+		checkNotEmpty = append(checkNotEmpty, dbName)
+	}
+
 	for c, s := range checkNotEmpty {
 		if len(s) <= 0 {
 			log.Fatalf("Parameter: %v was empty", checkNotEmptyName[c])
@@ -75,6 +85,114 @@ func main() {
 	iLog := &logger.StdOutLogger{}
 	iLog.SetLogLevel(logger.LogInfo)
 
+	if mode == "combine" {
+		combine(sess, iLog, remoteFS, &localFS)
+	} else if mode == "update" {
+		update(remoteFS, &localFS)
+	} else {
+		fatalError(errors.New("Unknown mode: " + mode))
+	}
+
+	printFinishStats()
+}
+
+func update(remoteFS fileaccess.FileAccess, localFS fileaccess.FileAccess) {
+	archivePrefix := "Archive/"
+	archived, err := remoteFS.ListObjects(destDataBucket, archivePrefix)
+	if err != nil {
+		fatalError(err)
+	}
+
+	rttToArchiveFiles := map[string][]string{}
+	for _, f := range archived {
+		f = f[len(archivePrefix):]
+		pos := strings.Index(f, "-")
+		if pos > 0 {
+			rtt := f[0:pos]
+			if files, ok := rttToArchiveFiles[rtt]; !ok {
+				rttToArchiveFiles[rtt] = []string{f}
+			} else {
+				files = append(files, f)
+				rttToArchiveFiles[rtt] = files
+			}
+		}
+	}
+
+	// Loop through and delete any with just 1 zip file, we don't need to optimise those
+	for rtt, files := range rttToArchiveFiles {
+		if len(files) < 2 {
+			delete(rttToArchiveFiles, rtt)
+		}
+	}
+
+	// Handle each file
+	for rtt, files := range rttToArchiveFiles {
+		updateForRTT(rtt, files, remoteFS)
+	}
+
+	fmt.Println(updateScript)
+}
+
+func updateForRTT(rtt string, allZips []string, remoteFS fileaccess.FileAccess) {
+	// See if we have any optimised files for this rtt
+	archOptPrefix := "Archive-Optimised/"
+	files, err := remoteFS.ListObjects(sourceDataBucket, archOptPrefix+rtt+"-")
+	if err != nil {
+		fmt.Printf("Failed to read archived files for rtt: %v, err: %v", rtt, err)
+		return
+	}
+
+	if len(files) < 1 {
+		fmt.Printf("No optimised archive for rtt: %v\n", rtt)
+		return
+	} else if len(files) > 1 {
+		fmt.Printf("Too many optimised archive for rtt: %v\n", rtt)
+		return
+	}
+
+	archivedZipName := files[0][len(archOptPrefix):]
+
+	// Make sure the optimised file matches one in the list
+	if !utils.ItemInSlice(archivedZipName, allZips) {
+		fmt.Printf("Optimised archive name doesn't match any source files for: %v\n", rtt)
+		return
+	}
+
+	// Delete all files older than or equal to this
+	optimisedTS, err := getTimestamp(archivedZipName)
+	if err != nil {
+		fmt.Printf("Failed to read timestamp for: %v", archivedZipName)
+		return
+	}
+
+	for _, f := range allZips {
+		needUpdate := false
+		if f == archivedZipName {
+			needUpdate = true
+		} else {
+			ts, err := getTimestamp(f)
+			if err != nil {
+				fmt.Printf("Failed to read timestamp for: %v", f)
+				return
+			}
+			needUpdate = ts < optimisedTS
+		}
+
+		if needUpdate {
+			updateScript += fmt.Sprintf("aws s3 rm s3://%v/%v\n", destDataBucket, "Archive/"+f)
+		}
+	}
+
+	// Copy the optimised file back
+	updateScript += fmt.Sprintf("aws s3 cp s3://%v/%v s3://%v/%v\n", sourceDataBucket, "Archive-Optimised/"+archivedZipName, destDataBucket, "Archive/")
+}
+
+func getTimestamp(fileName string) (int, error) {
+	_ /*expecting this to match already due to dir listing*/, timeStamp, err := datasetArchive.DecodeArchiveFileName(fileName)
+	return timeStamp, err
+}
+
+func combine(sess *session.Session, iLog logger.ILogger, remoteFS fileaccess.FileAccess, localFS fileaccess.FileAccess) {
 	// Connect to mongo
 	destMongoClient, _, err := mongoDBConnection.Connect(sess, destMongoSecret, iLog)
 	if err != nil {
@@ -101,6 +219,9 @@ func main() {
 
 	// Loop through all scans and read each archive one-by-one
 	workingDir, err := os.MkdirTemp("", "archive-fix-")
+	if err != nil {
+		log.Fatalln(err)
+	}
 
 	l := &logger.StdOutLogger{}
 
@@ -116,7 +237,7 @@ func main() {
 			l.Infof("============================================================")
 			l.Infof(">>> Downloading archives for scan: %v (%v)", scan.Title, scan.Id)
 
-			archive := datasetArchive.NewDatasetArchiveDownloader(remoteFS, &localFS, l, sourceDataBucket, "" /* not needed */)
+			archive := datasetArchive.NewDatasetArchiveDownloader(remoteFS, localFS, l, sourceDataBucket, "" /* not needed */)
 			_ /*localDownloadPath*/, localUnzippedPath, zipCount, lastZipName, err := archive.DownloadFromDatasetArchive(scan.Id, workingDir)
 			if err != nil {
 				log.Fatalf("Failed to download archive for scan %v: %v", scan.Id, err)
@@ -182,8 +303,6 @@ func main() {
 			// Delete all downloaded files and created zip
 		}
 	}
-
-	printFinishStats()
 }
 
 func getWithoutVersion(fileName string) string {
