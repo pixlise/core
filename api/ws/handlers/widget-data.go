5c4ecbc70274393c0852e2521716f8ecff8f1803
Ryan Stonebraker <ryan.a.stonebraker@jpl.nasa.gov>
2025-06-05 15:32:48 -0700

    Adds widget metadata write and get endpoints

--- api/ws/handlers/widget-data.go
+++ api/ws/handlers/widget-data.go
@@ -60,3 +60,65 @@ func HandleWidgetDataWriteReq(req *protos.WidgetDataWriteReq, hctx wsHelpers.Han
 		WidgetData: req.WidgetData,
 	}, nil
 }
+
+func HandleWidgetMetadataGetReq(req *protos.WidgetMetadataGetReq, hctx wsHelpers.HandlerContext) (*protos.WidgetMetadataGetResp, error) {
+	if req.Id == "" {
+		return nil, errors.New("widget metadata must have a predefined id to get")
+	}
+
+	result := hctx.Svcs.MongoDB.Collection(dbCollections.WidgetDataName).FindOne(context.TODO(), bson.M{
+		"_id": req.Id,
+	})
+
+	if result.Err() != nil {
+		return nil, result.Err()
+	}
+
+	widgetData := &protos.WidgetData{}
+	err := result.Decode(&widgetData)
+	if err != nil {
+		return nil, err
+	}
+
+	return &protos.WidgetMetadataGetResp{
+		Id:                req.Id,
+		WidgetName:        widgetData.WidgetName,
+		WidgetDescription: widgetData.WidgetDescription,
+	}, nil
+}
+
+func HandleWidgetMetadataWriteReq(req *protos.WidgetMetadataWriteReq, hctx wsHelpers.HandlerContext) (*protos.WidgetMetadataWriteResp, error) {
+	if req.Id == "" {
+		return nil, errors.New("widget metadata must have a predefined id to write to")
+	}
+
+	// Check if exists
+	result := hctx.Svcs.MongoDB.Collection(dbCollections.WidgetDataName).FindOne(context.TODO(), bson.M{
+		"_id": req.Id,
+	})
+
+	// The widget must already exist for us to write to it (created by screen configuration)
+	if result.Err() != nil {
+		return nil, result.Err()
+	}
+
+	// Update
+	_, err := hctx.Svcs.MongoDB.Collection(dbCollections.WidgetDataName).UpdateOne(context.TODO(), bson.M{
+		"_id": req.Id,
+	}, bson.M{
+		"$set": bson.M{
+			"widgetName":        req.WidgetName,
+			"widgetDescription": req.WidgetDescription,
+		},
+	})
+
+	if err != nil {
+		return nil, err
+	}
+
+	return &protos.WidgetMetadataWriteResp{
+		Id:                req.Id,
+		WidgetName:        req.WidgetName,
+		WidgetDescription: req.WidgetDescription,
+	}, nil
+}
