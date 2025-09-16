1bda7b6497007e47bf03c6e469172d5a45a832a7
Peter Nemere <peter.nemere@qut.edu.au>
2025-09-16 12:29:15 +1000

    Added ability to upload/download non-PMC related 3d points for a given image so we can show models on UI

---
+++ api/ws/handlers/image-3d-model-point.go
@@ -0,0 +1,77 @@
+package wsHandler
+
+import (
+	"context"
+	"errors"
+	"fmt"
+
+	"github.com/pixlise/core/v4/api/dbCollections"
+	"github.com/pixlise/core/v4/api/ws/wsHelpers"
+	"github.com/pixlise/core/v4/core/errorwithstatus"
+	protos "github.com/pixlise/core/v4/generated-protos"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+func HandleImage3DModelPointsReq(req *protos.Image3DModelPointsReq, hctx wsHelpers.HandlerContext) (*protos.Image3DModelPointsResp, error) {
+	ctx := context.TODO()
+	coll := hctx.Svcs.MongoDB.Collection(dbCollections.Image3DPointsName)
+	imgFound := coll.FindOne(ctx, bson.M{"_id": req.ImageName}, options.FindOne())
+	if imgFound.Err() != nil {
+		if imgFound.Err() == mongo.ErrNoDocuments {
+			return nil, errorwithstatus.MakeBadRequestError(fmt.Errorf("3D points not found for image: \"%v\"", req.ImageName))
+		}
+		return nil, errorwithstatus.MakeBadRequestError(fmt.Errorf("Failed to read 3d points for image \"%v\": %v", req.ImageName, imgFound.Err()))
+	}
+
+	pts := &protos.Image3DPoints{}
+	err := imgFound.Decode(pts)
+	if err != nil {
+		return nil, errorwithstatus.MakeBadRequestError(fmt.Errorf("Failed to decode 3d points for image \"%v\": %v", req.ImageName, err))
+	}
+
+	return &protos.Image3DModelPointsResp{
+		Points: pts,
+	}, nil
+}
+
+func HandleImage3DModelPointUploadReq(req *protos.Image3DModelPointUploadReq, hctx wsHelpers.HandlerContext) (*protos.Image3DModelPointUploadResp, error) {
+	if req.Points == nil {
+		return nil, errorwithstatus.MakeBadRequestError(errors.New("Points is empty"))
+	}
+
+	if len(req.Points.ImageName) <= 0 {
+		return nil, errorwithstatus.MakeBadRequestError(errors.New("Points.ImageName is not set"))
+	}
+
+	if len(req.Points.Points) <= 0 {
+		return nil, errorwithstatus.MakeBadRequestError(errors.New("Point list is empty"))
+	}
+
+	ctx := context.TODO()
+	coll := hctx.Svcs.MongoDB.Collection(dbCollections.ImagesName)
+	imgFound := coll.FindOne(ctx, bson.M{"_id": req.Points.ImageName}, options.FindOne())
+	if imgFound.Err() != nil {
+		if imgFound.Err() == mongo.ErrNoDocuments {
+			return nil, errorwithstatus.MakeBadRequestError(fmt.Errorf("Image \"%v\" not found", req.Points.ImageName))
+		}
+		return nil, errorwithstatus.MakeBadRequestError(fmt.Errorf("Failed to check image \"%v\": %v", req.Points.ImageName, imgFound.Err()))
+	}
+
+	// Request is valid, image exists, so lets store this
+	coll = hctx.Svcs.MongoDB.Collection(dbCollections.Image3DPointsName)
+
+	opt := options.Update().SetUpsert(true)
+
+	result, err := coll.UpdateByID(ctx, req.Points.ImageName, bson.D{{Key: "$set", Value: req.Points}}, opt)
+	if err != nil {
+		return nil, err
+	}
+
+	if result.MatchedCount == 0 && result.ModifiedCount == 0 {
+		hctx.Svcs.Log.Errorf("HandleImage3DModelPointUploadReq got unexpected upsert result: %+v", result)
+	}
+
+	return &protos.Image3DModelPointUploadResp{}, nil
+}
---
+++ internal/cmd-line-tools/api-integration-test/testImage3D.go
@@ -0,0 +1,82 @@
+package main
+
+import (
+	"github.com/pixlise/core/v4/core/client"
+	"github.com/pixlise/core/v4/core/wstestlib"
+)
+
+func testImage3DPoint(apiHost string) {
+	u1 := wstestlib.MakeScriptedTestUser(auth0Params)
+	u1.AddConnectAction("Connect", &client.ConnectInfo{
+		Host: apiHost,
+		User: test1Username,
+		Pass: test1Password,
+	})
+
+	u1.AddSendReqAction("Get non-existant points",
+		`{"image3DModelPointsReq":{"imageName": "non-existant.png"}}`,
+		`{
+			"msgId":1,
+			"status":"WS_BAD_REQUEST",
+			"errorText":"3D points not found for image: \"non-existant.png\"",
+			"image3DModelPointsResp":{}
+			}`,
+	)
+
+	u1.AddSendReqAction("Upload empty points (should fail)",
+		`{"image3DModelPointUploadReq":{"points": {"imageName": "non-existant.png"}}}`,
+		`{
+			"msgId":2,
+			"status":"WS_BAD_REQUEST",
+			"errorText":"Point list is empty",
+			"image3DModelPointUploadResp":{}
+		}`,
+	)
+
+	u1.AddSendReqAction("Upload points for non existant image",
+		`{"image3DModelPointUploadReq":{"points": {"imageName": "non-existant.png", "points": [{"x": 1, "y": 2, "z": 3}, {"x": 4, "y": 5, "z": 6}]}}}`,
+		`{
+			"msgId":3,
+			"status":"WS_BAD_REQUEST",
+  			"errorText": "Image \"non-existant.png\" not found",
+			"image3DModelPointUploadResp":{}
+		}`,
+	)
+
+	u1.AddSendReqAction("Upload points for real image",
+		`{"image3DModelPointUploadReq":{"points": {"imageName": "048300551/PCW_0125_0678031992_000RCM_N00417120483005510091075J02.png", "points": [{"x": 1, "y": 2, "z": 3}, {"x": 4, "y": 5, "z": 6}]}}}`,
+		`{
+			"msgId":4,
+			"status": "WS_OK",
+			"image3DModelPointUploadResp":{}
+		}`,
+	)
+
+	u1.AddSendReqAction("Get points for real image",
+		`{"image3DModelPointsReq":{"imageName": "048300551/PCW_0125_0678031992_000RCM_N00417120483005510091075J02.png"}}`,
+		`{
+			"msgId":5,
+			"status": "WS_OK",
+			"image3DModelPointsResp": {
+				"points": {
+					"imageName": "048300551/PCW_0125_0678031992_000RCM_N00417120483005510091075J02.png",
+					"points": [
+						{
+							"x": 1,
+							"y": 2,
+							"z": 3
+						},
+						{
+							"x": 4,
+							"y": 5,
+							"z": 6
+						}
+					]
+				}
+			}
+		}`,
+	)
+
+	u1.CloseActionGroup([]string{}, 5000)
+	wstestlib.ExecQueuedActions(&u1)
+}
