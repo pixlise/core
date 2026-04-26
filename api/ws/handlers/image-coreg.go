a6b67396b6869f266811be4c56ad05c0d7171808
Peter Nemere <peter.nemere@qut.edu.au>
2026-04-27 09:05:26 +1000

    Remvoed coreg because MarsViewer removed it over a year ago

--- api/ws/handlers/image-coreg.go
+++ api/ws/handlers/image-coreg.go
@@ -1,21 +1,12 @@
 package wsHandler
 
 import (
-	"github.com/pixlise/core/v4/api/coreg"
+	"fmt"
+
 	"github.com/pixlise/core/v4/api/ws/wsHelpers"
 	protos "github.com/pixlise/core/v4/generated-protos"
 )
 
 func HandleImportMarsViewerImageReq(req *protos.ImportMarsViewerImageReq, hctx wsHelpers.HandlerContext) (*protos.ImportMarsViewerImageResp, error) {
-	jobId := ""
-	var err error
-
-	jobId, err = coreg.StartCoregImport(req.TriggerUrl, hctx)
-	if err != nil {
-		return nil, err
-	}
-
-	return &protos.ImportMarsViewerImageResp{
-		JobId: jobId,
-	}, nil
+	return nil, fmt.Errorf("No longer implemented, coreg service was discontinued on MarsViewer side")
 }
