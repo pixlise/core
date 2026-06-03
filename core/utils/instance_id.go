7857ac0f13d9015c0e01201c67bc19e6ab1ce8d9
Peter Nemere <peter.nemere@qut.edu.au>
2026-06-03 15:32:34 +1000

    Fixing EC2 instance id request

--- core/utils/instance_id.go
+++ core/utils/instance_id.go
@@ -15,19 +15,41 @@ func GetInstanceId(timeoutSec int) string {
 	if err != nil {
 		fmt.Printf("Failed to create request for EC2 metdata instance_id: %v\n", err)
 	} else {
-		putReq.Header.Set("Content-Type", "application/octet-stream")
+		putReq.Header.Set("X-aws-ec2-metadata-token-ttl-seconds", "21600")
 
 		client := &http.Client{Timeout: time.Duration(timeoutSec) * time.Second}
 		putResponse, err := client.Do(putReq)
 		if err != nil {
-			fmt.Printf("Failed to request EC2 metdata instance_id: %v\n", err)
+			fmt.Printf("Failed to request EC2 metdata token: %v\n", err)
 		} else {
 			defer putResponse.Body.Close()
 			putBody, err := io.ReadAll(putResponse.Body)
 			if err != nil {
-				fmt.Printf("Failed to read EC2 metdata instance_id: %v\n", err)
+				fmt.Printf("Failed to read EC2 metdata token: %v\n", err)
 			} else if len(putBody) > 0 {
-				instanceId = string(putBody)
+				// We got the token, now send the actual request for instance id
+				token := string(putBody)
+
+				putReq, err = http.NewRequest("PUT", "http://169.254.169.254/latest/meta-data/instance-id", nil)
+				if err != nil {
+					fmt.Printf("Failed to create request for EC2 metdata instance_id: %v\n", err)
+				} else {
+					putReq.Header.Set("X-aws-ec2-metadata-token", token)
+
+					client := &http.Client{Timeout: time.Duration(timeoutSec) * time.Second}
+					putResponse, err := client.Do(putReq)
+					if err != nil {
+						fmt.Printf("Failed to request EC2 metdata instance_id: %v\n", err)
+					} else {
+						defer putResponse.Body.Close()
+						putBody, err := io.ReadAll(putResponse.Body)
+						if err != nil {
+							fmt.Printf("Failed to read EC2 metdata instance_id: %v\n", err)
+						} else if len(putBody) > 0 {
+							instanceId = string(putBody)
+						}
+					}
+				}
 			}
 		}
 	}
--- core/utils/instance_id_test.go
+++ core/utils/instance_id_test.go
@@ -4,5 +4,5 @@ func Example_utils_GetInstanceId() {
 	GetInstanceId(1)
 
 	// Output:
-	// Failed to request EC2 metdata instance_id: Put "http://169.254.169.254/latest/api/token": context deadline exceeded (Client.Timeout exceeded while awaiting headers)
+	// Failed to request EC2 metdata token: Put "http://169.254.169.254/latest/api/token": context deadline exceeded (Client.Timeout exceeded while awaiting headers)
 }
