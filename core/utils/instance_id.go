a5c079d60c0e1806dee184fa42f72b8aa8079a6f
Peter Nemere <peter.nemere@qut.edu.au>
2026-06-03 15:46:16 +1000

    Fixing EC2 instance id query

--- core/utils/instance_id.go
+++ core/utils/instance_id.go
@@ -1,6 +1,7 @@
 package utils
 
 import (
+	"bytes"
 	"fmt"
 	"io"
 	"net/http"
@@ -11,47 +12,59 @@ func GetInstanceId(timeoutSec int) string {
 	instanceId := RandStringBytesMaskImpr(16)
 
 	// If we are on EC2 we may be able to query its instance ID here
-	putReq, err := http.NewRequest("PUT", "http://169.254.169.254/latest/api/token", nil)
+	putReq, err := http.NewRequest("PUT", "http://169.254.169.254/latest/api/token", bytes.NewBuffer([]byte{}))
 	if err != nil {
 		fmt.Printf("Failed to create request for EC2 metdata instance_id: %v\n", err)
-	} else {
-		putReq.Header.Set("X-aws-ec2-metadata-token-ttl-seconds", "21600")
+		return instanceId
+	}
 
-		client := &http.Client{Timeout: time.Duration(timeoutSec) * time.Second}
-		putResponse, err := client.Do(putReq)
-		if err != nil {
-			fmt.Printf("Failed to request EC2 metdata token: %v\n", err)
-		} else {
-			defer putResponse.Body.Close()
-			putBody, err := io.ReadAll(putResponse.Body)
-			if err != nil {
-				fmt.Printf("Failed to read EC2 metdata token: %v\n", err)
-			} else if len(putBody) > 0 {
-				// We got the token, now send the actual request for instance id
-				token := string(putBody)
+	putReq.Header.Set("X-aws-ec2-metadata-token-ttl-seconds", "21600")
 
-				putReq, err = http.NewRequest("PUT", "http://169.254.169.254/latest/meta-data/instance-id", nil)
-				if err != nil {
-					fmt.Printf("Failed to create request for EC2 metdata instance_id: %v\n", err)
-				} else {
-					putReq.Header.Set("X-aws-ec2-metadata-token", token)
+	client := &http.Client{Timeout: time.Duration(timeoutSec) * time.Second}
+	putRespToken, err := client.Do(putReq)
+	if err != nil {
+		fmt.Printf("Failed to request EC2 metdata token: %v\n", err)
+		return instanceId
+	}
 
-					client := &http.Client{Timeout: time.Duration(timeoutSec) * time.Second}
-					putResponse, err := client.Do(putReq)
-					if err != nil {
-						fmt.Printf("Failed to request EC2 metdata instance_id: %v\n", err)
-					} else {
-						defer putResponse.Body.Close()
-						putBody, err := io.ReadAll(putResponse.Body)
-						if err != nil {
-							fmt.Printf("Failed to read EC2 metdata instance_id: %v\n", err)
-						} else if len(putBody) > 0 {
-							instanceId = string(putBody)
-						}
-					}
-				}
-			}
-		}
+	defer putRespToken.Body.Close()
+	putBodyToken, err := io.ReadAll(putRespToken.Body)
+	if err != nil {
+		fmt.Printf("Failed to read EC2 metdata token: %v\n", err)
+		return instanceId
+	}
+
+	if len(putBodyToken) <= 0 {
+		fmt.Printf("Token: %v\n", putBodyToken)
+		return instanceId
+	}
+
+	// We got the token, now send the actual request for instance id
+	token := string(putBodyToken)
+
+	putReq, err = http.NewRequest("PUT", "http://169.254.169.254/latest/meta-data/instance-id", bytes.NewBuffer([]byte{}))
+	if err != nil {
+		fmt.Printf("Failed to create request for EC2 metdata instance_id: %v\n", err)
+		return instanceId
+	}
+
+	putReq.Header.Set("X-aws-ec2-metadata-token", token)
+
+	putRespInst, err := client.Do(putReq)
+	if err != nil {
+		fmt.Printf("Failed to request EC2 metdata instance_id: %v\n", err)
+		return instanceId
+	}
+
+	defer putRespInst.Body.Close()
+	putBodyInst, err := io.ReadAll(putRespInst.Body)
+	if err != nil {
+		fmt.Printf("Failed to read EC2 metdata instance_id: %v\n", err)
+		return instanceId
+	}
+
+	if len(putBodyInst) > 0 {
+		instanceId = string(putBodyInst)
 	}
 
 	return instanceId
