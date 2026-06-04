0e24c058537d2de33104a3d59a9493f5f9507c27
Peter Nemere <peter.nemere@qut.edu.au>
2026-06-04 10:19:06 +1000

    Cleaned up instance id requesting and test

--- core/utils/instance_id.go
+++ core/utils/instance_id.go
@@ -9,14 +9,13 @@ import (
 	"github.com/aws/aws-sdk-go-v2/feature/ec2/imds"
 )
 
-func GetInstanceId(timeoutSec int) string {
+func GetInstanceId() (string, error) {
 	instanceId := RandStringBytesMaskImpr(16)
 
 	// Warning: this was AI generated
 	cfg, err := config.LoadDefaultConfig(context.TODO())
 	if err != nil {
-		fmt.Printf("Unable to load SDK config: %v\n", err)
-		return instanceId
+		return instanceId, fmt.Errorf("Unable to load SDK config: %v\n", err)
 	}
 
 	client := imds.NewFromConfig(cfg)
@@ -27,73 +26,15 @@ func GetInstanceId(timeoutSec int) string {
 
 	output, err := client.GetMetadata(context.TODO(), input)
 	if err != nil {
-		fmt.Printf("Failed to fetch instance ID from IMDS: %v\n", err)
-		return instanceId
+		return instanceId, fmt.Errorf("Failed to fetch instance ID from IMDS: %v\n", err)
 	}
 
 	instIdBody, err := io.ReadAll(output.Content)
 	if err != nil {
-		fmt.Printf("Failed to read instance ID from IMDS: %v\n", err)
-		return instanceId
+		return instanceId, fmt.Errorf("Failed to read instance ID from IMDS: %v\n", err)
 	}
 
 	instanceId = string(instIdBody)
-	/*
-		// If we are on EC2 we may be able to query its instance ID here
-		putReq, err := http.NewRequest("PUT", "http://169.254.169.254/latest/api/token", bytes.NewBuffer([]byte{}))
-		if err != nil {
-			fmt.Printf("Failed to create request for EC2 metdata instance_id: %v\n", err)
-			return instanceId
-		}
 
-		putReq.Header.Set("X-aws-ec2-metadata-token-ttl-seconds", "21600")
-
-		client := &http.Client{Timeout: time.Duration(timeoutSec) * time.Second}
-		putRespToken, err := client.Do(putReq)
-		if err != nil {
-			fmt.Printf("Failed to request EC2 metdata token: %v\n", err)
-			return instanceId
-		}
-
-		defer putRespToken.Body.Close()
-		putBodyToken, err := io.ReadAll(putRespToken.Body)
-		if err != nil {
-			fmt.Printf("Failed to read EC2 metdata token: %v\n", err)
-			return instanceId
-		}
-
-		if len(putBodyToken) <= 0 {
-			fmt.Printf("Token: %v\n", putBodyToken)
-			return instanceId
-		}
-
-		// We got the token, now send the actual request for instance id
-		token := string(putBodyToken)
-
-		putReq, err = http.NewRequest("PUT", "http://169.254.169.254/latest/meta-data/instance-id", bytes.NewBuffer([]byte{}))
-		if err != nil {
-			fmt.Printf("Failed to create request for EC2 metdata instance_id: %v\n", err)
-			return instanceId
-		}
-
-		putReq.Header.Set("X-aws-ec2-metadata-token", token)
-
-		putRespInst, err := client.Do(putReq)
-		if err != nil {
-			fmt.Printf("Failed to request EC2 metdata instance_id: %v\n", err)
-			return instanceId
-		}
-
-		defer putRespInst.Body.Close()
-		putBodyInst, err := io.ReadAll(putRespInst.Body)
-		if err != nil {
-			fmt.Printf("Failed to read EC2 metdata instance_id: %v\n", err)
-			return instanceId
-		}
-
-		if len(putBodyInst) > 0 {
-			instanceId = string(putBodyInst)
-		}
-	*/
-	return instanceId
+	return instanceId, nil
 }
--- core/utils/instance_id_test.go
+++ core/utils/instance_id_test.go
@@ -1,11 +1,16 @@
 package utils
 
-func Example_utils_GetInstanceId() {
-	GetInstanceId(1)
+import (
+	"fmt"
+	"strings"
+)
 
-	// If using HTTP requests, we got:
-	// Failed to request EC2 metdata token: Put "http://169.254.169.254/latest/api/token": context deadline exceeded (Client.Timeout exceeded while awaiting headers)
+func Example_utils_GetInstanceId() {
+	i, e := GetInstanceId()
+	fmt.Println(len(i) > 0)
+	fmt.Println(strings.HasPrefix(e.Error(), "Failed to fetch instance ID from IMDS: operation error ec2imds: GetMetadata, "))
 
 	// Output:
-	// Failed to fetch instance ID from IMDS: operation error ec2imds: GetMetadata, request canceled, context deadline exceeded
+	// true
+	// true
 }
