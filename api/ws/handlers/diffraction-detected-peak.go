f8a79480d86744be526f65e79947c576d57afd1e
Peter Nemere <peter.nemere@qut.edu.au>
2026-07-28 16:24:42 +1000

    Fixing build

--- api/ws/handlers/diffraction-detected-peak.go
+++ api/ws/handlers/diffraction-detected-peak.go
@@ -20,7 +20,7 @@ func HandleDetectedDiffractionPeaksReq(req *protos.DetectedDiffractionPeaksReq, 
 	}
 
 	// Cache the file locally, like we do with datasets (aka Scans)
-	diffRawData, err := wsHelpers.ReadDiffractionFile(req.ScanId, hctx.Svcs, true)
+	diffRawData, err := wsHelpers.ReadDiffractionFile(req.ScanId, hctx.Svcs)
 	if err != nil {
 		return nil, err
 	}
